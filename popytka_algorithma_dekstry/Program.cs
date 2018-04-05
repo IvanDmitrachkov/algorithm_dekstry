f98661f80ad6a525f7b031ce10c459378748f631
IvanDmitrachkov <dmitrachkovivan@gmail.com>
2018-04-05 18:09:12 +0300

    Добавьте файлы проекта.

---
+++ popytka_algorithma_dekstry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace popytka_algorithma_dekstry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
